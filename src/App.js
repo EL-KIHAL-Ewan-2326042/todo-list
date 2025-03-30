586d752c47f79afaf337db1aa397ce7ed17c1d25
Ewan EL KIHAL <ewan.eelk@gmail.com>
2025-03-30 22:32:07 +0200

    Quickfix

--- src/App.js
+++ src/App.js
@@ -433,7 +433,6 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <h1>Ma To-Do List ✅</h1>
 
           <div className="app-title-container">
             <h1>Ma To-Do List ✅</h1>
